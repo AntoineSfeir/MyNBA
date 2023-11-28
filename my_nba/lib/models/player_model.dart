b79d06c957a7bf40a3bd17071001946c2b359626
AntoineSfeir <antoinegsfeir@gmail.com>
2023-11-27 20:59:11 -0600

    pushing p

--- my_nba/lib/models/player_model.dart
+++ my_nba/lib/models/player_model.dart
@@ -1,28 +1,32 @@
 class Player {
   int playerID;
-  String name;
-  String team;
-  int jerseyNumber;
+  String firstName;
+  String lastName;
+  int height;
+  String teamID;
   String position;
-  String height;
+  int jerseyNumber;
 
-  Player(
-      {required this.playerID,
-      required this.name,
-      required this.jerseyNumber,
-      required this.position,
-      required this.team,
-      required this.height});
+  Player({
+    required this.playerID,
+    required this.firstName,
+    required this.lastName,
+    required this.height,
+    required this.teamID,
+    required this.position,
+    required this.jerseyNumber,
+  });
 
   // Convert a Player object to a map for database operations
   Map<String, dynamic> toMap() {
     return {
       'playerID': playerID,
-      'name': name,
-      'jerseyNumber': jerseyNumber,
+      'firstName': firstName,
+      'lastName': lastName,
+      'height': height,
+      'teamID': teamID,
       'position': position,
-      'team': team,
-      'height': height
+      'jerseyNumber': jerseyNumber,
     };
   }
 
@@ -30,11 +34,12 @@ class Player {
   factory Player.fromMap(Map<String, dynamic> map) {
     return Player(
       playerID: map['playerID'],
-      name: map['name'],
-      jerseyNumber: map['jerseyNumber'],
-      position: map['position'],
-      team: map['team'],
+      firstName: map['firstName'],
+      lastName: map['lastName'],
       height: map['height'],
+      teamID: map['teamID'],
+      position: map['position'],
+      jerseyNumber: map['jerseyNumber'],
     );
   }
 }
--- my_nba/lib/models/team_model.dart
+++ my_nba/lib/models/team_model.dart
@@ -2,8 +2,14 @@ class Team {
   String teamID;
   String city;
   String homeCourt;
+  String division;
 
-  Team({required this.teamID, required this.city, required this.homeCourt});
+  Team({
+    required this.teamID,
+    required this.city,
+    required this.homeCourt,
+    required this.division,
+  });
 
   // Convert a Team object to a map for database operations
   Map<String, dynamic> toMap() {
@@ -11,15 +17,17 @@ class Team {
       'teamID': teamID,
       'city': city,
       'homeCourt': homeCourt,
+      'division': division,
     };
   }
 
   // Create a Team object from a map received from the database
   factory Team.fromMap(Map<String, dynamic> map) {
     return Team(
-      teamID: map['name'],
+      teamID: map['teamID'],
       city: map['city'],
       homeCourt: map['homeCourt'],
+      division: map['division'],
     );
   }
 }
